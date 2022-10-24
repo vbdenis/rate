23209bbb66ef4b2e777f5d980415f56bb656103e
denis vybornov <vbdenis@yandex.ru>
2022-10-24 03:04:56 +0300

    init: config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import{ AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Rate App</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
